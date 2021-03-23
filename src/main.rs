fc65fd8c55023af6fef2d1603bc8c627555a0183
long <1845797136@qq.com>
2021-03-23 23:17:06 +0800

    首次提交

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io::{Error, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::str;
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
+    let mut buf = [0; 512];//定义一个数组
+    for _ in 0..1000 {//每个连接只处理1000次数据
+        let bytes_read = stream.read(&mut buf)?;//从流中读取数据并存储在buf中
+        if bytes_read == 0 {
+            return Ok(());
+        }
+        print!("从客户端接收到数据：{}",
+                 str::from_utf8(&buf[..bytes_read]).expect("Could not write buffer as string"));
+
+        stream.write(&buf[..bytes_read])?;//将从客户端接收到的数据发回客户端
+    }
+
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;//监听8080端口
+    let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
+
+    for stream in listener.incoming() {
+        let stream = stream.expect("failed!");
+        let handle = thread::spawn(move || {
+            handle_client(stream)
+                .unwrap_or_else(|error| eprintln!("{:?}", error));
+        });
+
+        thread_vec.push(handle);
+    }
+
+    for handle in thread_vec {
+        handle.join().unwrap();
+    }
+
+    Ok(())
+}
